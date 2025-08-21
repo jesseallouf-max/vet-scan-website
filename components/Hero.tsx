5caa7de0279dee3661f733e22d9ba3e737ee4e54
vetscannyc <jesseallouf@gmail.com>
2025-08-21 00:01:01 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -58,7 +58,7 @@ export default function Hero() {
           >
             <h1
               id="hero-heading"
-              className="text-xl sm:text-2xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight tracking-tight text-center md:text-left mb-4"
+              className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight tracking-tight text-center md:text-left mb-4"
             >
               <strong 
                 className="text-gray-900" 
@@ -68,7 +68,8 @@ export default function Hero() {
                   WebkitFontSmoothing: 'antialiased'
                 }}
               >
-                Veterinary Mobile Ultrasound Service
+                <span className="block">Veterinary Mobile</span>
+                <span className="block">Ultrasound Service</span>
               </strong>
             </h1>
            
